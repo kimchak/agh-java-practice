c6e87db24989e0caed14bee41d3dd003bee9e3ed
kimchak <60241771+kimchak@users.noreply.github.com>
2020-02-02 22:49:25 +0100

    Update Program4.java

--- Program4.java
+++ Program4.java
@@ -6,7 +6,7 @@ public class Program4 {
          int a;
          BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
          do {
-             System.out.print("Podaj liczbe od 1 do 6: "); 
+             System.out.print("Input a number between 1 and 6: "); 
                try
                {
                     a = Integer.parseInt(in.readLine());
@@ -14,46 +14,28 @@ public class Program4 {
                }
                catch (NumberFormatException e)
                {
-                 System.out.println("To nie byla liczba calkowita, sprobuj jeszcze raz.");
+                 System.out.println("Not an integer! Try again.");
                }
              } while (true);
          switch (a) {
          case 1:
-            System.out.println("Punkt (a=1)");
+            System.out.println("Point (a=1)");
             break;
          case 2:
-            System.out.println("Odcinek (a=2)");
+            System.out.println("Line segment (a=2)");
             break;
          case 3:
-            System.out.println("Trojkat (a=3)");
+            System.out.println("Triangle (a=3)");
             break;
          case 4:
-            System.out.println("Czworokat (a=4)");
+            System.out.println("Rectangle (a=4)");
             break;
          default:
-            System.out.println("Wielokat (a > 4)");
+            System.out.println("Polygon (a>4)");
             break;
          }
       } catch (IOException e) {
       }
    }
 }
-
-/*
- * Cwiczenia:
- * 1. dodaj kod sprawdzajacy czy podana liczba jest liczba typu int, np.:
- *                   
-                    do {
-                    System.out.print("Podaj liczbe od 1 do 6: "); 
-                      try
-                      {
-                           a = Integer.parseInt(in.readLine());
-                           break;
-                      }
-                      catch (NumberFormatException e)
-                      {
-                        System.out.println("To nie byla liczba calkowita, sprobuj jeszcze raz.");
-                      }
-                    } while (true);
- */
 
